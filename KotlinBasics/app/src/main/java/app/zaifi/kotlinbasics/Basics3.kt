8b5d90ffa4b817fca73fa6156545b30981c4774a
Huzaifa Shoaib <huzaifachughtai2001@gmail.com>
2024-02-03 20:42:06 +0500

    Nullables

--- KotlinBasics/app/src/main/java/app/zaifi/kotlinbasics/Basics3.kt
+++ KotlinBasics/app/src/main/java/app/zaifi/kotlinbasics/Basics3.kt
@@ -1,5 +1,18 @@
 package app.zaifi.kotlinbasics
 
-fun main(){
+// nullables
+fun main() {
+    var nullableName: String? = "Huzaifa"
+//    nullableName = null
 
+    // this is how we access the properties and methods of nullable variables
+    var len = nullableName?.length
+    nullableName?.let { println(it.length) }
+
+    // Elvis operator -> ?:
+    val name = nullableName ?: "Guest"
+    println("Name is $name")
+
+// not-null assertion -> !!
+    print(nullableName!!.lowercase())
 }
